ea6e9f22dba84096744e7b3dd063e04bf7804c6e
AnaniYang <18986852438@163.com>
2017-05-14 01:09:14 +0800

    renew
    Task update

---
+++ TideBike/TideBikeWeb/datas/nav.js
@@ -0,0 +1,90 @@
+var navs = [{
+    "title": "商品管理",
+    "icon": "fa-cubes",
+    "spread": true,
+    "children": [{
+        "title": "商城",
+        "icon": "&#xe641;",
+        "href": "mall.html"
+    }, {
+        "title": "配件",
+        "icon": "&#xe63c;",
+        "href": "bicycleAccessories.html"
+    }
+    , {
+        "title": "订单",
+        "icon": "&#xe63c;",
+        "href": "order.html"
+    }
+    //,{
+    //    "title": "导航",
+    //    "icon": "&#xe609;",
+    //    "href": "nav.html"
+    //}, {
+    //    "title": "Tab选项卡",
+    //    "icon": "&#xe62a;",
+    //    "href": "tab.html"
+    //}, {
+    //    "title": "辅助性元素",
+    //    "icon": "&#xe60c;",
+    //    "href": "auxiliar.html"
+    //}
+    ]
+}, {
+    "title": "客户管理",
+    "icon": "fa-cogs",
+    "spread": false,
+    "children": [{
+        "title": "基本信息",
+        "icon": "fa-table",
+        "href": "customer.html"
+    }
+    //, {
+    //    "title": "Navbar组件",
+    //    "icon": "fa-navicon",
+    //    "href": "navbar.html"
+    //}, {
+    //    "title": "Laytpl+Laypage",
+    //    "icon": "&#xe628;",
+    //    "href": "paging.html"
+    //}
+    ]
+}, {
+    "title": "新闻资讯",
+    "icon": "&#x1002;",
+    "spread": false,
+    "children": [{
+        "title": "资讯信息",
+        "icon": "fa-check-square-o",
+        "href": "new.html"
+    }]
+}, {
+    "title": "地址本",
+    "icon": "fa-address-book",
+    "href": "",
+    "spread": false,
+    "children": [{
+        "title": "Github",
+        "icon": "fa-github",
+        "href": "htmlpreview.github.io/https://github.com/AnaniYang/bicycle.git/"
+    }, {
+        "title": "QQ",
+        "icon": "fa-qq",
+        "href": "http://www.qq.com/"
+    }, {
+        "title": "Fly社区",
+        "icon": "&#xe609;",
+        "href": "http://fly.layui.com/"
+    }, {
+        "title": "新浪微博",
+        "icon": "fa-weibo",
+        "href": "http://weibo.com/"
+    }]
+}
+//, {
+//    "title": "这是一级导航",
+//    "icon": "fa-stop-circle",
+//    "href": "https://www.baidu.com",
+//    "spread": false
+//}
+];
